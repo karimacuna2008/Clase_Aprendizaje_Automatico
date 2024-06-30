c4923e33b63a0bef92829a89d4222d3cbb059f47
karimacuna2008 <karimacuna2008@gmail.com>
2024-06-30 14:23:01 -0600

    update

--- Practicas/Practica_1/Practica 1.py
+++ Practicas/Practica_1/Practica 1.py
@@ -1,2 +1,20 @@
+#   Instalación de librerias
+#   pip install pandas
+#   pip install matplotlib
+#   pip install seaborn
+
 #   Librerias
 import pandas as pd
+
+#   Carga de información - Prework
+url = "https://raw.githubusercontent.com/karimacuna2008/Clase_Aprendizaje_Automatico/main/Practicas/Practica_1/typed_uanl.csv"
+df = pd.read_csv(url)
+print(df.describe())
+
+#   Estadísticas descriptivas
+for columna in df.columns:
+    descripcion = df[columna].describe()
+    print(f"Estadísticas descriptivas de '{columna}':")
+    print(descripcion)
+    print()
+
